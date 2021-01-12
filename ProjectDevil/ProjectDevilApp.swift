abd2496dacf40b9ccb7f60d971b6176ebe764eea
George Filatov <liggidarck@gmail.com>
2021-01-13 00:26:02 +0300

    Initial Commit

---
+++ ProjectDevil/ProjectDevilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDevilApp.swift
+//  ProjectDevil
+//
+//  Created by George Filatov on 13.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDevilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
