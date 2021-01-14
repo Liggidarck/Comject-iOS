f23d31d6b4fc78d87bf46f634cd4f54905404f33
George Filatov <liggidarck@gmail.com>
2021-01-15 02:05:18 +0300

    Добавлен ProfileView, Обновлен HomeView

--- ProjectDevil/MainViews/HomeView.swift
+++ ProjectDevil/MainViews/HomeView.swift
@@ -9,24 +9,118 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationView{
-            ZStack{
-                ScrollView(.vertical){
-                    VStack{
-                        HStack{
-                            Text("My Work")
-                                .font(.system(size: 19, weight: .bold, design: .default))
-                            Spacer()
-                        }
-                        
-                        
-                        
-                    }
-                    .padding()
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    MainProjectCard()
                 }
             }
+            
             .navigationTitle("Home")
+            .padding()
         }
+    }
+}
+
+struct MainProjectCard: View {
+    var body: some View {
+        VStack {
+            
+            //Картинка проекта
+            HStack {
+                Image("4")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipped()
+            }
+            
+            //Текст проекта
+            VStack(alignment: .leading) {
+                
+                HStack {
+                    Text("MY MAIN PROJECT")
+                        .font(.system(size: 14, weight: .light, design: .default))
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Gradient Notes")
+                        .font(.system(size: 28, weight: .bold, design: .default))
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Long default text for you project.  Long continuation of the description. Мой уровень английского не позволяет сделать мне более длинный текст на английском, поэтому продолжу на русском чтобы текст описания выглядил более объёмно.")
+                        .font(.system(size: 17, weight: .light, design: .default))
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("#Информатика #Программирование #IT #Мобильное приложение #что-то еще #длинноеслово ")
+                        .font(.system(size: 14, weight: .light, design: .default))
+                        .foregroundColor(Color.blue)
+                    Spacer()
+                }
+                
+            }
+            
+            //Лайки комменты
+            VStack {
+                HStack(spacing: 20.0) {
+                    HStack {
+                        Image("like_border")
+                            .resizable()
+                            .frame(width: 25, height: 25, alignment: .center)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Text("3.4k")
+                            .font(.system(size: 14, weight: .light, design: .default))
+                    }
+                    
+                    HStack {
+                        Image("chat")
+                            .resizable()
+                            .frame(width: 25, height: 25, alignment: .center)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Text("1.3k Comments")
+                            .font(.system(size: 14, weight: .light, design: .default))
+                    }
+                    
+                    Spacer()
+                }
+                
+                //Кнопки
+                HStack{
+                    Spacer()
+
+                    Button(action: {
+                        print("Publish Clicked!") //Это типа логи
+                    }, label: {
+                        Text("Publish").font(.system(size: 17))
+                    })
+                    Spacer()
+
+                    Button(action: {
+                        print("Edit Project Clicked!") //Это типа логи
+                    }, label: {
+                        Text("Edit Project").font(.system(size: 17))
+                    })
+                    Spacer()
+                    
+                }.padding(.top)
+                
+            }
+            
+            
+        }
+        
     }
 }
 
--- ProjectDevil/MainViews/ProfileView.swift
+++ ProjectDevil/MainViews/ProfileView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Image("3")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                ava()
+                    .frame(width: 170, height: 170, alignment: .center)
+                    .offset(x: -20, y: -85)
+                    .padding(.bottom, -120)
+                Spacer()
+            }
+            
+            HStack {
+                
+                VStack(alignment: .leading) {
+                    Text("George Filatov")
+                        .font(.title)
+                    Text("liggidarck")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    //Вот сюда добавить нажание на кнопку
+                    print("Нажата кнопка Edit Profile в ProfileView")
+                }, label: {
+                    Text("EDIT PROFILE")
+                })
+                
+                Spacer()
+                
+            }.padding(.leading)
+            
+            profile_data()
+            Spacer()
+            
+        }
+    }
+    
+    
+}
+
+struct ava: View {
+    var body: some View{
+        Image("ava_me")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 6))
+    }
+}
+
+struct profile_data: View {
+    var body: some View {
+        Text("4")
     }
 }
 
